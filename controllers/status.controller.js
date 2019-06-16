566fe830cc00ea128378e1be27c7f62d246f6aad
Samarth Gupta <samarth.guptas.1995@gmail.com>
2019-06-16 10:58:39 +0530

    added controller to handle requests at home page

--- controllers/status.controller.js
+++ controllers/status.controller.js
@@ -2,7 +2,6 @@ const Status = require('../models/status.model')
 
 var id
 
-
 exports.saveSlots = function (req, res) {
   let status = new Status(
     {
@@ -26,7 +25,7 @@ exports.saveSlots = function (req, res) {
 exports.freeSlots = function (req, res) {
   Status.findById(id, function (err, status) {
     if (err) return next(err)
-    console.log("====>  /freeSlots call at : " + Date.now() + "from IP Address : " + req.ip)
+    console.log("====>  /freeSlots call at : " + Date.now() + " from IP Address : " + req.ip)
     res.send(status.unoccupiedSlot)
   })
 }
@@ -35,7 +34,12 @@ exports.freeSlots = function (req, res) {
 exports.occupiedSlots = function (req, res) {
    Status.findById(id, function (err, status) {
     if (err) return next(err)
-    console.log("====>  /occupiedSlots call at : " + Date.now() + "from IP Address : " + req.ip)    
+    console.log("====>  /occupiedSlots call at : " + Date.now() + " from IP Address : " + req.ip)    
     res.send(status.occupiedSlot)
   })
+}
+
+exports.home = function (req, res) {
+  console.log("==========> Someone navigated to homepage at : " + Date.now())
+  res.send("You have landed the home page, please navigate to appropriate API")
 }
--- routes/status.route.js
+++ routes/status.route.js
@@ -6,7 +6,8 @@ const controller = require('../controllers/status.controller')
 
 // a simple test url to check that all of our files are communicating correctly.
 router.post('/saveSlots', controller.saveSlots)
-router.get('/freeSlots/', controller.freeSlots)
+router.get('/freeSlots', controller.freeSlots)
 router.get('/occupiedSlots', controller.occupiedSlots)
+router.get('/', controller.home)
 
 module.exports = router
